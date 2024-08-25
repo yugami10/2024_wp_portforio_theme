2c3a1b408141ff40b67168205788476d30efe697
yugami10 <yugami333@gmail.com>
2024-08-25 15:27:30 +0900

    固定ページをベストプラクティスに寄せた書き方に修正（page-<slag名>）

--- header.php
+++ header.php
@@ -11,3 +11,19 @@
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
     <?= wp_head() ?>
 </html>
+
+<header class="header">
+    <div class="header__img_wrapper">
+        <img src="<?= get_template_directory_uri() ?>/images/logo.png" alt="logo" />
+    </div>
+    <nav class="header__nav">
+        <ul>
+            <li>
+                <a href="<?= home_url('result') ?>">実績</a>
+            </li>
+            <li>
+                <a href="<?= home_url('contact') ?>"">お問い合わせ</a>
+            </li>
+        </ul>
+    </nav>
+</header>
