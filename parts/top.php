917c1ae617df2822e1f71ffe9d15068112cb0e0f
yugami10 <yugami333@gmail.com>
2024-08-25 17:04:42 +0900

    メインビジュアルを作成

--- parts/top.php
+++ parts/top.php
@@ -1,1 +1,11 @@
-topだよ
+<main>
+    <div class="main_visual">
+        <div class="main_visual__img">
+            <p class="main_visual__message">
+                Web制作を通じて感謝されたい！！
+                <br />
+                承認欲求モンスター
+            </p>
+        </div>
+    </div>
+</main>
