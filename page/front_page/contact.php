8ff6e8832ebf0a9d33bd027bbaac7b2066085d7f
yugami10 <yugami333@gmail.com>
2024-08-28 08:25:02 +0900

    自己紹介とお問い合わせを軽く作成

--- page/front_page/contact.php
+++ page/front_page/contact.php
@@ -1,4 +1,5 @@
 <section class="contact section-self-style">
     <h2 class="section-title">ご依頼について</h2>
+    <p>お問い合わせフォームからお気軽にご連絡ください。</p>
     <?= includeButton(home_url('contact'), '問い合わせてみる') ?>
 </section>
--- page/front_page/self_introduction.php
+++ page/front_page/self_introduction.php
@@ -1,3 +1,19 @@
 <section class="self-introduction section-self-style">
     <h2 class="section-title">自己紹介</h2>
+    <div class="self-introduction__content_wrapper">
+        <p class="self-introduction__content">
+            はじめまして。東京在住の山田涼介です。
+            <br />
+            過去、電気設計とwebアプリ開発のお仕事に従事してきました。
+            <br />
+            <br />
+            現在はお仕事の幅を広げてweb制作とLP制作の開発・デザインのお仕事を行っていこうと活動しています。
+            <br />
+            <br />
+            お客様とはコミュニケーションを密にとることで、頭の中でふわっとしていたイメージを具体的な形に落とし込んで提供させていただきます。
+        </p>
+        <div class="self-introduction__self_picture_wrapper">
+            <img src="<?= get_template_directory_uri() . "/images/jisseki_1.png" ?>" alt="self_picture" />
+        </div>
+    </div>
 </section>
