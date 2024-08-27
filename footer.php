be98cdd9d01eff3c9fe9071509effa96d4a0f918
yugami10 <yugami333@gmail.com>
2024-08-28 08:32:24 +0900

    軽めにフッター作成

--- footer.php
+++ footer.php
@@ -1,4 +1,10 @@
-wareふったーなり
+<footer class="footer">
+    <!-- <div class="footer__left_pictures"></div> -->
+    <div class="footer__content">
+        <p>copyright</p>
+    </div>
+    <!-- <div class="footer__right_pictures"></div> -->
+</footer>
 
 
 <?php
